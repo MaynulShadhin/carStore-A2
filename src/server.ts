58b29d659922a61a66e52d5a9f60d29a7ef19aee
Md. Maynul Islam <islammaynul82@gmail.com>
2024-11-23 01:38:54 +0600

    eslint and prettier setup done

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 import config from './app/config';
+import app from './app';
 
 async function main() {
-  await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-app.listen(config.port, () => {
-  console.log(`app listening on port ${config.port}`);
-});
+main();
